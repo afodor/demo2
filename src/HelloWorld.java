630c1b77c9434ca5600201bea0ab0e6120d433c6
afodor <anthony.fodor@gmail.com>
2023-08-25 13:33:28 -0400

    Update HelloWorld.java
    this is a change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld
 
 public static void main(String[] args )
 {
-	System.out.println("Hello");
+	System.out.println("Goodbye");
+
+	for( int x=0; x < 100; x++)
+		System.out.println("Goodbye");
+
 }
 
 }
